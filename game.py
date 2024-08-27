5856e0a61ec13b0f463f6ae75e40b6c51d4574c1
Rei <161365537+MyaRLCon27@users.noreply.github.com>
2024-08-27 05:10:50 -0700

    Link can walk now

--- game.py
+++ game.py
@@ -1,15 +1,16 @@
 from pygame_functions import *
 
 screenSize(1024,768)
-
+setAutoUpdate(False)
 link = makeSprite("LinkSimple.png", 14)
 showSprite(link)
-moveSprite(link, 500, 350)
-
+Link_X = 500
+Link_Y = 350
+moveSprite(link, Link_X, Link_Y)
 nextFrame = clock()
 frame = 0
 orientation = 0
-
+Link_Speed = 6
 
 while True:
     if clock() >nextFrame:
@@ -19,21 +20,30 @@ while True:
         
         if keyPressed("down"):
             changeSpriteImage(link, 0*2 + frame)
-            orientation =0    
+            orientation =0
+            Link_Y = Link_Y + Link_Speed
+            
         if keyPressed("up"):
             changeSpriteImage(link, 1*2 + frame)
             orientation =1
+            Link_Y = Link_Y - Link_Speed
+            
         if keyPressed("right"):
             changeSpriteImage(link, 2*2 + frame)
             orientation =2
+            Link_X = Link_X + Link_Speed
+            
         if keyPressed("left"):
             changeSpriteImage(link, 3*2 + frame)
             orientation =3
+            Link_X = Link_X - Link_Speed
+            
         if keyPressed("space"):
             changeSpriteImage(link, orientation + 8)
         if keyPressed("h"):
             changeSpriteImage(link, frame+12)
-            
-# test
+        moveSprite(link, Link_X, Link_Y)
+        updateDisplay()
+
 
 endWait()
